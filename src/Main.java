d24d7b0d6e204a1e9b02b79a0fc6d64e464a661e
Michał Reclik <michal-reclik@wp.pl>
2020-03-22 13:36:21 +0100

    bang

--- src/Main.java
+++ src/Main.java
@@ -279,6 +279,7 @@ public class Main {
         }
     }
     public static void mergesort(int[] arr, int beg, int end) {
+        comparisons++;
         if (beg < end) {
             int m = beg + (end - beg) / 2;
 
@@ -286,6 +287,83 @@ public class Main {
             mergesort(arr, m + 1, end);
 
             merge(arr, beg, m, end);
+        }
+    }
+
+    //hybrid
+    public static void merge_obj(Comparable[] arr, int beg, int mid, int end){
+        int i, j, k;
+        int n1 = mid-beg+1;
+        int n2 = end-mid;
+
+        Comparable[] tmp1, tmp2;
+        tmp1 = new Comparable[n1];
+        tmp2 = new Comparable[n2];
+
+        for (i = 0; i < n1; i++) {
+            tmp1[i] = arr[beg + i];
+            swaps++;
+        }
+        for (j = 0; j < n2; j++) {
+            tmp2[j] = arr[mid + 1 + j];
+            swaps++;
+        }
+
+        i = 0;
+        j = 0;
+        k = beg;
+        while (i < n1 && j < n2) {
+            if (tmp1[i].compareTo(tmp2[j])<1) {
+                arr[k] = tmp1[i];
+                i++;
+                comparisons++;
+            } else {
+                arr[k] = tmp2[j];
+                j++;
+                comparisons++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = tmp1[i];
+            i++;
+            k++;
+            comparisons++;
+        }
+
+        while (j < n2) {
+            arr[k] = tmp2[j];
+            j++;
+            k++;
+            comparisons++;
+        }
+    }
+    public static void hybrid(Comparable[] arr, int beg, int end){
+        if (end - beg > 40) {
+            int m = beg + (end - beg) / 2;
+
+            hybrid(arr, beg, m);
+            hybrid(arr, m + 1, end);
+
+            merge_obj(arr, beg, m, end);
+        } else {
+            int q = 1;
+            int p = 0;
+            Comparable tmp;
+            while (q < arr.length) {
+                comparisons++;
+                while (q - 1 - p >= 0 && arr[q - 1 - p].compareTo(arr[q - p])>0) {
+                    comparisons++;
+                    swaps+=3;
+                    tmp = arr[q - p];
+                    arr[q - p] = arr[q - p - 1];
+                    arr[q - p - 1] = tmp;
+                    p++;
+                }
+                q++;
+                p = 0;
+            }
         }
     }
 
@@ -442,5 +520,17 @@ public class Main {
 //        System.out.println(Arrays.toString(arg));
 //
 //        merge(arg, arg2);
+
+        Comparable[] myArr = new Comparable[5];
+        myArr[0]="agh";
+        myArr[1]="aaparatus";
+        myArr[2]="xavery";
+        myArr[3]="bobo";
+        myArr[4]="string";
+
+        System.out.println("\ncomparing: " + myArr[1].compareTo(myArr[4]));
+        System.out.println(Arrays.toString(myArr));
+        hybrid(myArr, 0, myArr.length-1);
+        System.out.println(Arrays.toString(myArr));
     }
 }
